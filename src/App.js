5523136a36816719ab0334e6acfc31f6383ea0f4
mythily-muthu <mythilimuthu18@gmail.com>
2023-01-12 14:38:15 +0530

    Feat: removed the unwanted files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import EmpListing from "./EmpListing";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import EmpCreate from "./EmpCreate";
+import EmpEdit from "./EmpEdit";
+import EmpDetails from "./EmpDetails";
+
+
 
 
 function App() {
   return (
     <div className="App">
-      hi
-    </div>
+      <h2 className='text-center'>React CRUD application</h2>
+      <Router>
+        <Routes>
+          <Route path="/" element={<EmpListing />} />
+          <Route path="/employee/create" element={<EmpCreate />} />
+          <Route path="/employee/edit/:empid" element={<EmpEdit />} />
+          <Route path="/employee/details/:empid" element={<EmpDetails />} />
+
+        </Routes>
+      </Router>
+    </div >
   );
 }
 
