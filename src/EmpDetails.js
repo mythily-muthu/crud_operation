b4323806e5c071b189e0c674ff3dc41a91c7ea03
mythily-muthu <mythilimuthu18@gmail.com>
2023-01-14 13:01:10 +0530

    Feat: dreated empDetails component

--- src/EmpDetails.js
+++ src/EmpDetails.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const EmpDetails = () => {
+
+    let emp_id = useParams()
+    let [empData, setEmpData] = useState([])
+
+    const getAllEmployees = async () => {
+        let res = await axios.get("http://localhost:8000/employees" + emp_id);
+        setEmpData(res.data);
+    };
+    console.log(empData)
+    useEffect(() => {
+        getAllEmployees();
+    }, []);
+
     return (
-        <div>EmpDetails</div>
+        <div>
+            {empData && <h1>the emp name is : {empData.name}</h1>}
+        </div>
     )
 }
 
